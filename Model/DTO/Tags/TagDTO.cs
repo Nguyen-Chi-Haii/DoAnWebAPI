568ef6b443ba2a2a19f797850301185122a0e3c1
ChiHaiNguyen <Haiforwork2005@gmail.com>
2025-10-02 20:47:21 +0700

    TagAPI

--- Model/DTO/Tags/TagDTO.cs
+++ Model/DTO/Tags/TagDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace DoAnWebAPI.Model.DTO.Tag
+﻿using DoAnWebAPI.Model.DTO.Image;
+
+namespace DoAnWebAPI.Model.DTO.Tag
 {
     public class TagDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
-    }
+        
+        public List<ImageDTO> Images { get; set; }
+        }
 }
